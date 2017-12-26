1fb5a4464c7ca2b9a832321be971c68d0439c611
lyrad <lyradb@gmail.com>
2017-12-26 10:30:44 +0800

    working code, just like an email.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "/home/odoo/devs/addons/DTS",
+    'name': "Document Tracking System",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','hr'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/dts_document_security.xml',
+        'security/ir.model.access.csv',
+        'views/document_tracking_views.xml',
+        'views/menuitems.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ models/document_tracking.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    department_id = fields.Many2one('hr.department', string='Office')
+
+class HrDepartment(models.Model):
+    _inherit = 'hr.department'
+
+    name = fields.Char('Office Name', required=True)
+    parent_id = fields.Many2one('hr.department', string='Office Department', index=True)
+    dept_code = fields.Char(string="Office Code", required=False, help="Will be use for Document Code.")
+
+class DtsDocumentType(models.Model):
+    _name = 'dts.document.type'
+
+    name = fields.Char('Document Type', required=True)
+    active = fields.Boolean(string="Active?", default=True)
+
+class DtsDocument(models.Model):
+    _name = 'dts.document'
+    _description = 'Memo Document'
+
+    name = fields.Char(string="Name", required=False, default='New Document')
+    transaction_date = fields.Datetime(string="Transaction Date", required=False, default=fields.datetime.today(), readonly="1")
+    sender_id = fields.Many2one(comodel_name="hr.employee", string="Sender",default=lambda self: self.env.uid, readonly=True)
+    sender_office_id = fields.Many2one(comodel_name="hr.department", string="Sender Office", related = 'sender_id.department_id', readonly=True)
+
+    # # Recipient
+    document_type_id = fields.Many2one(comodel_name="dts.document.type", string="Document Type", required=True, domain=[('active', '=', True)])
+    subject = fields.Char(string="Subject", required=True, )
+    message = fields.Text(string="Message", required=False, )
+    recipient_office_id = fields.Many2one(comodel_name="hr.department", string="Recipient Office", required=True)
+    recipient_id = fields.Many2one(comodel_name="hr.employee", string="Recipient", required=True, domain="[('department_id','=',recipient_office_id)]")
+
+    attachment_number = fields.Integer(compute='_get_attachment_number', string="Number of Attachments")
+    attachment_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'dts.document')], string='Attachments')
+
+    tracking_type = fields.Selection(string="Tracking Type",
+                                           selection=[
+                                               ('incoming', 'Incoming'),
+                                               ('outgoing', 'Outgoing'),
+                                           ], default='outgoing')
+
+    state = fields.Selection(string="state",
+                                     selection=[
+                                         ('draft', 'Draft'),
+                                         ('cancel', 'Cancelled'),
+                                         ('send', 'Sent'),
+                                         ('read', 'Read'),
+                                         ('receive', 'Received'),
+                                     ], default='draft')
+    
+
+
+
+    @api.multi
+    def _get_attachment_number(self):
+        read_group_res = self.env['ir.attachment'].read_group(
+            [('res_model', '=', 'dts.document'), ('res_id', 'in', self.ids)],
+            ['res_id'], ['res_id'])
+        attach_data = dict((res['res_id'], res['res_id_count']) for res in read_group_res)
+        for record in self:
+            record.attachment_number = attach_data.get(record.id, 0)
+
+
+    @api.multi
+    def action_get_attachment_tree_view(self):
+        attachment_action = self.env.ref('base.action_attachment')
+        action = attachment_action.read()[0]
+        action['context'] = {'default_res_model': self._name, 'default_res_id': self.ids[0]}
+        action['domain'] = str(['&', ('res_model', '=', self._name), ('res_id', 'in', self.ids)])
+        action['search_view_id'] = (self.env.ref('dts.ir_attachment_view_search_inherit_dts_document').id, )
+        return action
+
+    @api.model
+    def create(self, values):
+        values['name'] = 'Documents: '+ self.document_type_id.name
+
+        return super(DtsDocument, self).write(values)
+
+
+    @api.multi
+    def action_send(self):
+        return self.write({'state': 'send', 'tracking_type': 'outgoing'})
+
+    @api.multi
+    def action_received(self):
+        return self.write({'state': 'receive'})
