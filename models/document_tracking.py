47cd337fe871ca595b1fc04f977acbdd17f099f9
lyrad <lyradb@gmail.com>
2017-12-27 20:03:36 +0800

    halfway.

--- models/document_tracking.py
+++ models/document_tracking.py
@@ -23,12 +23,24 @@ class DtsDocumentType(models.Model):
 class DtsDocument(models.Model):
     _name = 'dts.document'
     _description = 'Memo Document'
+    _order = 'transaction_date desc'
+
+    def _get_employee_id(self):
+        user_id = self.env.user.id
+        res_resource = self.env['resource.resource'].search([('user_id', '=', user_id),('active', '=', True)],order="id desc",limit=1)
+        if not res_resource and user_id != 1:
+            raise ValidationError("No related employee assigned to current user. Please consult System Admin")
+        else:
+            resource_id = res_resource.id
+            res_employee = self.env['hr.employee'].search([('resource_id', '=', resource_id),('active', '=', True)])
+            employee_id = res_employee.id
+
+        return employee_id
 
     name = fields.Char(string="Name", required=False, default='New Document')
     transaction_date = fields.Datetime(string="Transaction Date", required=False, default=fields.datetime.today(), readonly="1")
-    sender_id = fields.Many2one(comodel_name="hr.employee", string="Sender",default=lambda self: self.env.uid, readonly=True)
-    sender_office_id = fields.Many2one(comodel_name="hr.department", string="Sender Office", related = 'sender_id.department_id', readonly=True)
-
+    sender_id = fields.Many2one(comodel_name="hr.employee", string="Sender", default=_get_employee_id, readonly=True, related_sudo=True)
+    sender_office_id = fields.Many2one(comodel_name="hr.department", string="Sender Office", required=True, related_sudo=True,related='sender_id.department_id', readonly="1")
     # # Recipient
     document_type_id = fields.Many2one(comodel_name="dts.document.type", string="Document Type", required=True, domain=[('active', '=', True)])
     subject = fields.Char(string="Subject", required=True, )
@@ -37,7 +49,8 @@ class DtsDocument(models.Model):
     recipient_id = fields.Many2one(comodel_name="hr.employee", string="Recipient", required=True, domain="[('department_id','=',recipient_office_id)]")
 
     attachment_number = fields.Integer(compute='_get_attachment_number', string="Number of Attachments")
-    attachment_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'dts.document')], string='Attachments')
+    attachment_ids = fields.One2many('ir.attachment','res_id', domain=[('res_model', '=', 'dts.document')], string='Attachments')
+    receive_date = fields.Datetime(string="Received Date", required=False, readonly="1")
 
     tracking_type = fields.Selection(string="Tracking Type",
                                            selection=[
@@ -53,9 +66,18 @@ class DtsDocument(models.Model):
                                          ('read', 'Read'),
                                          ('receive', 'Received'),
                                      ], default='draft')
-    
 
+    recipient_user_id = fields.Many2one(comodel_name="res.users", string="Recipient User", readonly=True, related="recipient_id.user_id", store=False)
+    current_user = fields.Many2one(comodel_name="res.users", string="Current User", compute="_get_current_user", store=False)
 
+    @api.depends()
+    def _get_current_user(self):
+        self.update({'current_user':self.env.user.id})
+
+    @api.depends()
+    def _hide_accept(self):
+        ret = self.recipient_id.user_id.id - self.env.user.id
+        self.update({'hide_accept': ret})
 
     @api.multi
     def _get_attachment_number(self):
@@ -76,17 +98,35 @@ class DtsDocument(models.Model):
         action['search_view_id'] = (self.env.ref('dts.ir_attachment_view_search_inherit_dts_document').id, )
         return action
 
-    @api.model
-    def create(self, values):
-        values['name'] = 'Documents: '+ self.document_type_id.name
-
-        return super(DtsDocument, self).write(values)
+    # @api.model
+    # def create(self, values):
+    #     values['name'] = 'Documents: '+ self.document_type_id.name
+    #
+    #     return super(DtsDocument, self).write(values)
 
 
     @api.multi
     def action_send(self):
+        self.send_to_channel('Hi! Please see '+self.document_type_id.name+' with subject: '+self.subject, self.recipient_id.user_id)
         return self.write({'state': 'send', 'tracking_type': 'outgoing'})
 
     @api.multi
     def action_received(self):
-        return self.write({'state': 'receive'})
+        self.send_to_channel('Hi! I accepted your '+self.document_type_id.name+' with subject: '+self.subject, self.sender_id.user_id)
+        return self.write({'state': 'receive','receive_date':fields.datetime.today()})
+
+    def send_to_channel(self, body, to_user):
+        users = to_user
+        ch_obj = self.env['mail.channel']
+        if users:
+            for user in users:
+                ch_name = user.name + ', ' + self.env.user.name
+                ch = ch_obj.sudo().search([('name', 'ilike', str(ch_name))])
+                if not ch:
+                    ch = ch_obj.sudo().search([('name', 'ilike', str(self.env.user.name + ', ' + user.name))])
+                ch.message_post(attachment_ids=[], body=body, content_subtype='html',
+                message_type='comment', partner_ids=[], subtype='mail.mt_comment',
+                email_from=self.env.user.partner_id.email, author_id=self.env.user.partner_id.id)
+
+        return True
+
